6a97f99245e6010d266d4b7b5b489c9d9458e353
Alex Tacescu <alextac98@gmail.com>
2019-01-29 12:22:12 -0500

    Add changes to the project

--- src/main/java/com/neuronrobotics/smallkat/JavaIKModel.java
+++ src/main/java/com/neuronrobotics/smallkat/JavaIKModel.java
@@ -18,66 +18,78 @@ public class JavaIKModel implements DhInverseSolver {
 	@Override
 	public double[] inverseKinematics(TransformNR target, double[] jointSpaceVector, DHChain chain) {
 		System.out.println("My IK");
-		double[] inv = new double[jointSpaceVector.length];
-		try {
-			ArrayList<DHLink> links = chain.getLinks();
-			// THis is the jacobian for the given configuration
-			// Matrix jacobian = chain.getJacobian(jointSpaceVector);
-			Matrix taskSpacMatrix = target.getMatrixTransform();
+        try {
+            ArrayList<DHLink> links = chain.getLinks();
+            // THis is the jacobian for the given configuration
+            //Matrix jacobian =  chain.getJacobian(jointSpaceVector);
+            Matrix taskSpacMatrix = target.getMatrixTransform();
 
-			int linkNum = jointSpaceVector.length;
+            int linkNum = jointSpaceVector.length;
 
-			double x = target.getX();
-			double y = target.getY();
-			double z = target.getZ();
+            double x = target.getZ();
+            double y = target.getY();
+            double z = target.getX();
 
-			double ang = 0;
-			System.out.println(target.getRotation().toString());
+            System.out.println("x: " + x);
+            System.out.println("y: " + y);
+            System.out.println("z: " + z);
 
-			double l1_d = Math.abs(links.get(0).getR());
-			double l2_d = Math.abs(links.get(1).getR());
-			double l3_d = Math.abs(links.get(2).getR());
-			double l4_d = Math.abs(links.get(3).getR());
+            double ang = 0;
 
-			double theta1 = Math.atan(y / Math.abs(z));
 
-			double r1 = Math.sqrt(Math.pow(z, 2) + Math.pow(y, 2));
-			double x1 = r1;
-			double y1 = x;
-			double Px = x1 - l4_d * Math.sin(ang);
-			double Py = y1 - l4_d * Math.cos(ang);
-			// Make below negative to switch to other angle
-			double theta3_1 = Math.acos(((Math.pow(Px, 2) + Math.pow(Py, 2)) - (Math.pow(l2_d, 2) + Math.pow(l3_d, 2)))
-					/ (2 * l2_d * l3_d));
-			double theta3_2 = theta3_1 * -1;
+            //System.out.println(target.getRotation().toString());
 
-			double B = Math.atan(Py / Px);
-			double Y = Math.acos((Math.pow(Px, 2) + Math.pow(Py, 2) + Math.pow(l2_d, 2) - Math.pow(l3_d, 2))
-					/ (2 * l2_d * Math.sqrt(Math.pow(Px, 2) + Math.pow(Py, 2))));
+            double l1_d = links.get(0).getR();
+            double l2_d = links.get(1).getR();
+            double l3_d = links.get(2).getR();
+            double l4_d = links.get(3).getR();
 
-			double theta2_1 = B + Y;
-			double theta2_2 = B - Y;
+            double[] inv = new double[linkNum];
 
-			double theta4_1 = (Math.PI / 2 - ang) - (theta2_1 + theta3_1);
-			double theta4_2 = (Math.PI / 2 - ang) - (theta2_2 + theta3_2);
+            double theta1 = Math.atan(y / Math.abs(z));
 
-			System.out.println(theta1);
-			System.out.println(theta2_1);
-			System.out.println(theta3_1);
-			System.out.println(theta4_1);
+            double r1 = Math.sqrt(Math.pow(z, 2) + Math.pow(y,2));
+            double x1 = r1;
+            double y1 = x;
+            double Px = x1 - l4_d * Math.sin(ang);
+            double Py = y1 - l4_d * Math.cos(ang);
+            // Make below negative to switch to other angle
+            double theta3_1 = Math.acos(((Math.pow(Px, 2) + Math.pow(Py,2)) - (Math.pow(l2_d,2) + Math.pow(l3_d, 2))) / (2 * l2_d * l3_d));
+            double theta3_2 = theta3_1 * -1;
 
-			inv[0] = 0;// theta1;
-			inv[1] = 0;// theta2_1;
-			inv[2] = 0;// theta3_1;
-			inv[3] = 0;// theta3_1;
+            double B = Math.atan(Py / Px);
+            double Y = Math.acos((Math.pow(Px, 2) + Math.pow(Py, 2) + Math.pow(l2_d, 2) - Math.pow(l3_d, 2)) / (2 * l2_d * Math.sqrt(Math.pow(Px, 2) + Math.pow(Py, 2))));
 
-			System.out.println("\r\n\r\nJoint Vector = " + inv + "\r\n\r\n");
+            double theta2_1 = B + Y;
+            double theta2_2 = B - Y;
 
-			return inv;
-		} catch (Throwable t) {
-			BowlerStudio.printStackTrace(t);
-		}
-		return inv;
+            double theta4_1 = (Math.PI / 2 - ang) - (theta2_1 + theta3_1);
+            double theta4_2 = (Math.PI / 2 - ang) - (theta2_2 + theta3_2);
+
+			//theta1 = 90;
+			//theta2_1 = 0;
+			//theta3_1 = 0;
+			//theta4_1 = 0;
+			
+            System.out.println(theta1);
+            System.out.println(theta2_1);
+            System.out.println(theta3_1);
+            System.out.println(theta4_1);
+		
+			
+            //inv[0] = theta1; inv[1] = theta2_1; inv[2] = theta3_1; inv[3] = theta4_1;
+            inv[0] = Math.toDegrees(theta1);
+            inv[1] = Math.toDegrees(theta2_1);
+            inv[2] = Math.toDegrees(theta3_1);
+            inv[3] = Math.toDegrees(theta4_1);
+
+            System.out.println("\r\n\r\nJoint Vector = " + inv + "\r\n\r\n");
+
+            return inv;
+        } catch (Throwable t) {
+            BowlerStudio.printStackTrace(t);
+            return null;
+        }
 	}
 
 }
